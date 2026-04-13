711aaed9bb8b3ca5cc0138f7f6b40f69d0e9fc8c
BethelStack <YOUR-GITHUB-EMAIL>
2026-04-13 16:25:21 +0300

    Replace with todoList

---
+++ script.js
@@ -0,0 +1,61 @@
+const todoList = [
+  {
+    name: "Study JS",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "Finish the project",ej
+    dueDate: "2022-12-22",
+  },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    // const { name, dueDate } = todoObject;
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = `
+    <div class="todo-item">
+
+      <div class="todo-name">${name}</div>
+      <div class="todo-date">${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-button">Delete</button> 
+      </div>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
+document
+  .querySelector(".js-add-todo-button")
+  .addEventListener("click", addTodo);
